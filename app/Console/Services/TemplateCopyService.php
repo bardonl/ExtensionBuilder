96e16d1235dad13fb20fb855071f8f902bcfeafe
bardo-nl <degeus@redkiwi.nl>
2017-04-07 15:17:50 +0200

    [TASK] Changed placeholders in the ts files [TASK] Made check to see if it is for an ts file

--- app/Console/Services/TemplateCopyService.php
+++ app/Console/Services/TemplateCopyService.php
@@ -26,6 +26,8 @@ class TemplateCopyService
         if (!$this->dependencyInjectionManager()->getFileSystem()->exists(realpath('../') . '/' . $config['path'][0] . $key . '.php')) {
             if (array_key_exists('typoScript', $config) && $config['typoScript'] == 1) {
                 $this->copyRootTemplates($key, $config);
+            } else {
+                $this->copyTemplates($key, $config);
             }
         } else {
 
@@ -82,11 +84,19 @@ class TemplateCopyService
             TEMPLATE_DIRECTORY . '/' . $key,
             realpath('../') . '/' . $config['path'] . $key
         );
+        
+        $name = 'ExtensionName';
+
+        if ($this->dependencyInjectionManager()->getFileSystem()->extension(realpath('../') . '/' . $config['path'] . $key) == 'ts'){
+            $name = 'extension_name';
+            $extensionKey = array_filter(array_map('strtolower',preg_split('/(?=[A-Z])/',$config['extensionKey'])));
+            $config['extensionKey'] = implode('_', $extensionKey);
+        }
 
         $this->dependencyInjectionManager()->getFileReplaceUtility()->findAndReplace(
             realpath('../') . '/' . $config['path'] . $key,
             [
-                'ExtensionName' => $config['extensionKey']
+                $name => $config['extensionKey']
             ]
         );
 
